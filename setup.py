a79fde80b434064ad1517b793bd52fb01f830f4d
vkrivtsov <vkrivtsov@gamenuts.ru>
2018-12-21 19:02:57 +0300

    add nltk 3.4

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ extra_deps["all"] = sorted(set(sum(extra_deps.values(), [])))
 
 
 setup(
-    name='biblios',
+    name='biblos',
     version='0.0.1',
     python_requires=">=3.7",
     packages=find_packages(),
     install_requires=[
         "toolz ~= 0.9",
-        "click ~= 7.0"
+        "click ~= 7.0",
+        "nltk ~= 3.4"
     ],
     extras_require=extra_deps,
     entry_points={
