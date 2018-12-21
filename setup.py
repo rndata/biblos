716cd7b01112110f99611f041b90ebe47d59839c
cvb <ccvb3291@gmail.com>
2018-12-21 18:21:45 +0300

    Add project stuff

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+extra_deps = dict()
+extra_deps["all"] = sorted(set(sum(extra_deps.values(), [])))
+
+
+setup(
+    name='biblios',
+    version='0.0.1',
+    python_requires=">=3.7",
+    packages=find_packages(),
+    install_requires=[
+        "toolz ~= 0.9",
+        "click ~= 7.0"
+    ],
+    extras_require=extra_deps,
+    entry_points={
+        "console_scripts": [
+        ],
+    }
+)
